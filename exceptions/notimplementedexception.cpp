ab4db8e97e4a6cd7800e71e397a3854f7467702f
Dieter <phodiale@ya.ru>
2022-06-03 22:31:41 +0300

    fix warnings

--- exceptions/notimplementedexception.cpp
+++ exceptions/notimplementedexception.cpp
@@ -3,11 +3,16 @@
 #include "toolsmacro.h"
 using namespace Tools;
 
+namespace
+{
 CONST_LITERAL ExceptionName("NotImplementedException");
+}
 
 NotImplementedException::NotImplementedException(const char* file, const char* function, int lineNum) noexcept:
     ToolLibException(ExceptionName)
 {
-
+    Q_UNUSED(file)
+    Q_UNUSED(function)
+    Q_UNUSED(lineNum)
 }
 
